ba26070cce9df606d6384f83eb03f89b9a69b153
Anita Lee <anita.lee@gmail.com>
2022-05-11 21:16:00 -0400

    updated db seed file, updated small tag on footer

--- seed.py
+++ seed.py
@@ -8,15 +8,51 @@ i1 = Image(
     exif_height=500,
     exif_width=700,
     exif_camera_model="iPhone 12 Pro",
-    url="https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg?cs=srgb&dl=pexels-wojciech-kumpicki-2071873.jpg&fm=jpg"
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A55%3A21.605527.jpg"
 )
 
 i2 = Image(
     exif_height=1000,
     exif_width=1200,
     exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/eric.png"
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A55%3A29.730320.jpg"
 )
 
-db.session.add_all([i1, i2])
+i3 = Image(
+    exif_height=1000,
+    exif_width=1200,
+    exif_camera_model=None,
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A58%3A15.648381.jpg"
+)
+
+i4 = Image(
+    exif_height=1000,
+    exif_width=1200,
+    exif_camera_model=None,
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A01%3A49.232730.jpg"
+)
+
+i5 = Image(
+    exif_height=1000,
+    exif_width=1200,
+    exif_camera_model=None,
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A02%3A10.172556.jpg"
+)
+
+i6 = Image(
+    exif_height=1000,
+    exif_width=1200,
+    exif_camera_model=None,
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A08%3A22.723306.jpg"
+)
+
+i7 = Image(
+    exif_height=1000,
+    exif_width=1200,
+    exif_camera_model=None,
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A09%3A59.553793.jpg"
+)
+
+
+db.session.add_all([i1, i2, i3, i4, i5, i6, i7])
 db.session.commit()
