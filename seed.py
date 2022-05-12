a41cd90aae39157ee558e47991e79fb8de170d06
Anita Lee <anita.lee@gmail.com>
2022-05-12 15:51:39 -0400

    updated seed db file

--- seed.py
+++ seed.py
@@ -8,49 +8,50 @@ i1 = Image(
     exif_height=500,
     exif_width=700,
     exif_camera_model="iPhone 12 Pro",
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A55%3A21.605527.jpg"
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A40%3A47.389471.jpg"
+
 )
 
 i2 = Image(
     exif_height=1000,
     exif_width=1200,
-    exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A55%3A29.730320.jpg"
+    exif_camera_model="iPhone 12 Pro",
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A40%3A58.761935.jpg"
 )
 
 i3 = Image(
     exif_height=1000,
     exif_width=1200,
-    exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1120%3A58%3A15.648381.jpg"
+    exif_camera_model="iPad Pro",
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A41%3A06.324822.jpg"
 )
 
 i4 = Image(
     exif_height=1000,
     exif_width=1200,
-    exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A01%3A49.232730.jpg"
+    exif_camera_model="Samsung Galaxy S22",
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A41%3A15.567303.jpg"
 )
 
 i5 = Image(
     exif_height=1000,
     exif_width=1200,
-    exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A02%3A10.172556.jpg"
+    exif_camera_model="Google Pixel 6 Pro",
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A41%3A26.104517.jpg"
 )
 
 i6 = Image(
     exif_height=1000,
     exif_width=1200,
-    exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A08%3A22.723306.jpg"
+    exif_camera_model="Samsung Galaxy S22",
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A41%3A33.120265.jpg"
 )
 
 i7 = Image(
     exif_height=1000,
     exif_width=1200,
     exif_camera_model=None,
-    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1121%3A09%3A59.553793.jpg"
+    url="https://pixlyrithm25.s3.amazonaws.com/2022-05-1215%3A41%3A41.362821.jpg"
 )
 
 
